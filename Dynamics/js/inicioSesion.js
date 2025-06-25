809419d6f06113fddcdde12f0ebb7fe4d0ad780b
GaelzonMS <324244315@alumno.enp.unam.mx>
2025-06-24 23:13:50 -0600

    se realizo uan replica de login en crear cuenta y se empezo el JS de login

--- Dynamics/js/inicioSesion.js
+++ Dynamics/js/inicioSesion.js
@@ -1,2 +1,5 @@
 /* En esta sección se hara la creacion de una cuenta */
 
+const input_usuario = document.getElementById("usuario");
+const input_contraseña = document.getElementById("contraseña")
+const submit_btn = document.getElementById("iniciar")
