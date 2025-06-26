42e3838e8920da65b561b622fd23f47ff332ee24
GaelzonMS <324244315@alumno.enp.unam.mx>
2025-06-26 10:37:49 -0600

    Se crearon los archivos para la creacion de playlist y se añadio un nuevo form para ello

---
+++ Dynamics/js/crearPlaylist.js
@@ -0,0 +1,8 @@
+/* Archivo que creara un playlist dentro de la sidebar
+** Este servira para todas las vistas*/
+
+let btn_crearPlaylist = document.getElementById("btnAgregar");
+
+btn_crearPlaylist.addEventListener("click", ()=>{
+    
+})
