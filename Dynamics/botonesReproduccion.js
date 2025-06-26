f3f7496dbc92c63bb36f7f6b6a272bba55d2f410
MaFerRosaE <324302268@alumno.enp.unam.mx>
2025-06-25 23:17:48 -0600

    Los botones tienen iconos, el boton de playPausa funciona y la barra de duracion. Id especifica

---
+++ Dynamics/botonesReproduccion.js
@@ -0,0 +1,63 @@
+let player;
+let duracion = 0;
+let updateInterval;
+let videoId= "cWppAbqm9I8";
+
+const inputDuracion = document.getElementById("duracion");
+const playPauseBtn = document.getElementById("playPausa");
+let playPauseImg= document.getElementById("playImg");
+//const duracion = document.getElementById("duracion");
+
+function onPlayerReady(event) {
+    duracion = player.getDuration();
+    player.playVideo();
+    inputDuracion.max = duracion;
+
+    updateInterval = setInterval(() => {
+        if (player && player.getPlayerState() === YT.PlayerState.PLAYING) {
+            inputDuracion.value = player.getCurrentTime();
+        }
+    }, 1000);
+}
+
+function onPlayerStateChange(event){
+    if (event.data == YT.PlayerState.PLAYING) {
+        playPauseImg.setAttribute("src", "../Statics/media/play-solid.png");
+    } 
+    else if (event.data == YT.PlayerState.PAUSED || event.data == YT.PlayerState.ENDED) {
+            playPauseImg.setAttribute("src", "../Statics/media/pause-solid.png");
+        }
+    if (event.data === YT.PlayerState.ENDED) {
+        clearInterval(updateInterval);
+    }
+}
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+        videoId: "cWppAbqm9I8",
+        playerVars: {
+            controls: 0,
+            modestbranding: 1,
+            rel: 0,
+            showinfo: 0,
+            fs: 0,
+        },
+        events: {
+            onReady: onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        },
+    });
+}
+
+playPauseBtn.addEventListener("click", () => {
+    let state = player.getPlayerState();
+    if (state === YT.PlayerState.PLAYING) 
+        player.pauseVideo(); 
+    else 
+        player.playVideo();
+});
+
+inputDuracion.addEventListener("input", () => {
+    let seekTo = inputDuracion.value;
+    player.seekTo(seekTo, true);
+});
