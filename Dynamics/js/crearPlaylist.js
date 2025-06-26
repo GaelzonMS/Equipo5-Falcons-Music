d5f5e613f21b4320a7e27bf1a06212866ab87644
GaelzonMS <324244315@alumno.enp.unam.mx>
2025-06-26 11:33:54 -0600

    commit provisional, no me acuerdo q cambio

--- Dynamics/js/crearPlaylist.js
+++ Dynamics/js/crearPlaylist.js
@@ -2,7 +2,8 @@
 ** Este servira para todas las vistas*/
 
 let btn_crearPlaylist = document.getElementById("btnAgregar");
+let inp_newPlaylist = document.getElementById("agregar-container");
 
 btn_crearPlaylist.addEventListener("click", ()=>{
-    
+    inp_newPlaylist.setAttribute("display", "");
 })
