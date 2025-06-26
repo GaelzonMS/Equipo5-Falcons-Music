661ec7c54324d696b2d79c14b631f85ae3709147
MaFerRosaE <324302268@alumno.enp.unam.mx>
2025-06-26 17:43:01 -0600

    Se eliminaron los console.log, se agregaron comentarios

--- Dynamics/botonesReproduccion.js
+++ Dynamics/botonesReproduccion.js
@@ -1,67 +1,73 @@
-let player;
-let duracion = 0;
+let player; //REPRODUCTOR API
+let duracion = 0;//DURACION DEL VIDEO
 let updateInterval;
-let videoId= "cWppAbqm9I8";
-let barrasMostrar= 1;
-let artista= document.getElementById("artistaCancionR");
-let nomCancion= document.getElementById("cancionReproducida");
-let imagCancion= document.getElementById("imagCancion");
-let barrasBtn= document.getElementById("colaReproduccion");
-let playPauseImg= document.getElementById("playImg");
-let colaReproduccion= document.getElementById("colaRepro");
-let artistaCR= document.getElementById("artistaCancionCR");
-let cancionCR= document.getElementById("cancionReproducidaCR");
-let albumImgCR= document.getElementById("albumImag");
-let nomCR =document.querySelectorAll(".nomCR");
-let artCR =document.querySelectorAll(".artCR");
-let imgBtnPlayCr= document.querySelectorAll(".imgBtnPlayCr");
-let listaReproduccion= [];
+let videoId= "";//ID INICIAL DEL VIDEO
+let barrasMostrar= 1;//SI SE MOSTRARÁ O NO LA COLA DE REPRODUCCION
+let listaReproduccion= [];//LISTA DE REPRODUCCION
 
-const canciones = baseDatosJSON.canciones;
-const numCanciones= canciones.length;
-const artistas= baseDatosJSON.artistas;
-const albumes= baseDatosJSON.album;
-const generos= baseDatosJSON.genero;
-const inputDuracion = document.getElementById("duracion");
-const playPauseBtn = document.getElementById("playPausa");
+let artista= document.getElementById("artistaCancionR");//NOMBRE DEL ARTISTA EN EL FOOTER
+let nomCancion= document.getElementById("cancionReproducida");//NOMBRE DE LA CANCIÓN EN EL FOOTER
+let imagCancion= document.getElementById("imagCancion");//IMAGEN DEL ÁLBUM DE LA CANCIÓN ACTUAL EN EL FOOTER
+let barrasBtn= document.getElementById("colaReproduccion");//BOTON DE LA COLA DE REPRODUCCIÓN EN EL FOOTER
+let playPauseImg= document.getElementById("playImg");//IMAGEN DEL BOTON DE PLAYPAUSA
+const playPauseBtn = document.getElementById("playPausa");//BOTON DE PAUSA
+const inputDuracion = document.getElementById("duracion");//INPUT TIPO RANGE. BARRA DE DURACIÓN
 
-function setColaReproducción(maxLista, espacio){
+let colaReproduccion= document.getElementById("colaRepro");//ASIDE DE LA COLA DE REPRODUCCIÓN
+let artistaCR= document.getElementById("artistaCancionCR");//NOMBRE DEL ARTISTA EN EL TÍTULO DE LA COLA DE REPRODUCCIÓN
+let cancionCR= document.getElementById("cancionReproducidaCR");//NOMBRE DE LA CANCIÓN EN EL TÍTULO DE LA COLA DE REPRODUCCIÓN
+let albumImgCR= document.getElementById("albumImag");//IMAGEN DEL ALBUM EN LA COLA DE REPRODUCCIÓN
+
+let nomCR =document.querySelectorAll(".nomCR");//TODOS LOS NOMBRES DE LAS CANCIONES EN LA COLA DE REPRODUCCIÓN
+let artCR =document.querySelectorAll(".artCR");//TODOS LOS NOMBRES DE LOS ARTISTAS EN LA COLA DE REPRODUCCIÓN
+let imgBtnPlayCr= document.querySelectorAll(".imgBtnPlayCr");//TODAS LAS IMÁGENES DE LOS ALBUMES EN LAS CANCIONES DE LA COLA DE REPRODUCCIÓN
+//IDEA ADDEVENTLISTENER PARA CADA BOTON Y QUE CAMBIE UNA VARIABLE DEPENDIENDO EL NO. DE CANCIÓN
+const canciones = baseDatosJSON.canciones;//CANCIONES DE LA BASE DE DATOS
+const numCanciones= canciones.length;//NUM DE CANCIONES DE LA BASE DE DATOS
+const artistas= baseDatosJSON.artistas;//ARTISTAS DE LA BASE DE DATOS
+const albumes= baseDatosJSON.album;//ALBUMES DE LA BASE DE DATOS
+
+//FUNCIÓN QUE ESTABLECE LAS CANCIONES EN LA COLA DE REPRODUCCIÓN
+function setColaReproducción(maxLista, espacio){//PARAMETROS DE NUM DE CANCIONES A ASIGNAR Y EL ESPACIO QUE OCUPARAN EN LISTA DE REPRODUCCIÓN
     for (let i=0; i < maxLista; i++){
-        let r= Math.floor(Math.random() * (numCanciones));
-        while (listaReproduccion.indexOf(r) != -1){
-            r= Math.floor(Math.random() * (numCanciones));
+        let r= Math.floor(Math.random() * (numCanciones));//NÚMERO ALEATORIO RANGO 0 AL TOTAL DE CANCIONES
+        while (listaReproduccion.indexOf(r) != -1){//WHILE PARA EVITAR REPETICIONES
+            r= Math.floor(Math.random() * (numCanciones));//REASIGNAR UN VALOR SI YA EXISTE LA CANCIÓN
         }
-        let a = canciones[r].id_album - 1;
-        listaReproduccion[espacio]= r;
+        let a = canciones[r].id_album - 1;//INDICE DEL ALBUM
+        listaReproduccion[espacio]= r;//ASIGNAR VALOR DE LA LISTA DE REPRODUCCIÓN
+        //ASIGNAR EL NOMBRE DEL ARTISTA, LA CANCIÓN Y LA IMAGEN DEL ALBUM
         artCR[i].textContent= `${canciones[r].artista}`;
         nomCR[i].textContent= `${canciones[r].nombre}`;
         imgBtnPlayCr[i].setAttribute("src", `${albumes[a].url_img}`);
-        console.log(artCR[i], nomCR[i], imgBtnPlayCr[i]);
-        espacio += 1;
+        espacio += 1;//HACER QUE EL VALOR AUMENTE
     }
-    console.log(listaReproduccion);
 }
-function setBtnCancion(){
-    let r=listaReproduccion[0];
-    let a= canciones[r].id_album -1;
+function setBtnCancion(){//FUNCIÓN QUE SE ACTIVA AL HACER CLICK A LOS BOTONES DE COLA DE REPRODUCCIÓN
+    let r=listaReproduccion[0];//VALOR DEL INDICE DE LA LISTA DE REPRODUCCIÓN
+    let a= canciones[r].id_album -1;//INDICE DEL ALBUM
+    //ASIGNAR EL NOMBRE DEL ARTISTA, LA CANCIÓN Y LA IMAGEN DEL ALBUM TANTO EN EL FOOTER COMO EN EL ASIDE
     artista.textContent= `${canciones[r].artista}`;
     artistaCR.textContent= `${canciones[r].artista}`;
     nomCancion.textContent= `${canciones[r].nombre}`;
     cancionCR.textContent= `${canciones[r].nombre}`;
+    imagCancion.setAttribute("src", `${albumes[a].url_img}`);
+    albumImgCR.setAttribute("src", `${albumes[a].url_img}`);
+    //CAMBIAR ID DEL VIDEO 
     player.loadVideoById(canciones[r].link);
+    //CAMBIAR LA DURACIÓN DEL VIDEO
     updateInterval = setInterval(() => {
         duracion = player.getDuration();
         inputDuracion.max = duracion;
     }, 1000);
-    imagCancion.setAttribute("src", `${albumes[a].url_img}`);
-    albumImgCR.setAttribute("src", `${albumes[a].url_img}`);
 }
 
 function onPlayerReady(event) {
+    //OBTENER DURACIÓN DEL PRIMER ALBUM
     duracion = player.getDuration();
     player.playVideo();
     inputDuracion.max = duracion;
-
+    //ESTABLECER DURACIÓN
     updateInterval = setInterval(() => {
         if (player && player.getPlayerState() === YT.PlayerState.PLAYING) {
             inputDuracion.value = player.getCurrentTime();
@@ -71,18 +77,20 @@ function onPlayerReady(event) {
 
 function onPlayerStateChange(event){
     if (event.data == YT.PlayerState.PLAYING) {
+    //SI SE ESTÁ REPRODUCIENDO, PON EL ÍCONO DE REPRODUCCIÓN
         playPauseImg.setAttribute("src", "../Statics/media/play-solid.png");
-    } 
+    } //SI SE PARO LA REPRODUCCIÓN, PON EL ÍCONO DE PAUSA
     else if (event.data == YT.PlayerState.PAUSED || event.data == YT.PlayerState.ENDED) {
             playPauseImg.setAttribute("src", "../Statics/media/pause-solid.png");
-        }
+        }//SI TERMINO DE REPRODUCIRSE, LIMPIA LA BARRA Y AGREGA OTRA CANCIÓN A LISTA DE REPRODUCCIÓN
     if (event.data === YT.PlayerState.ENDED) {
         clearInterval(updateInterval);
-        setColaReproducción(1, 3);
+        setColaReproducción(1, listaReproduccion.length);
     }
 }
 
 function onYouTubeIframeAPIReady() {
+    //AGREGA EL OBJETO PLAYER CON DISTINTOS ATRIBUTOS
     player = new YT.Player("player", {
         videoId: `${videoId}`,
         playerVars: {
@@ -97,32 +105,34 @@ function onYouTubeIframeAPIReady() {
             'onStateChange': onPlayerStateChange
         },
     });
-    setColaReproducción(3, 0);
+    setColaReproducción(3, 0);//AL INICIAR, ESTABLECE TRES CANCIONES A LA COLA
 }
 
+//SI SE HACE CLICK EN EL BOTON DE PAUSA, PAUSA O HAS PLAY
 playPauseBtn.addEventListener("click", () => {
     let state = player.getPlayerState();
     if (state === YT.PlayerState.PLAYING) 
-        player.pauseVideo(); 
+        player.pauseVideo();
     else 
         player.playVideo();
 });
 
+//SI CAMBIAS LA DURACIÓN, QUE REPRODUZCA LA DURACIÓN DESDE DÓNDE SE ENCUENTRA
 inputDuracion.addEventListener("input", () => {
     let seekTo = inputDuracion.value;
     player.seekTo(seekTo, true);
 });
 
+//SI SE HACE CLICK EN LAS BARRAS DE LA COLA DE REPRODUCIÓN
 barrasBtn.addEventListener("click", () => {
-    console.log("si jala");
+    //CAMBIA LAS VARIABLES
     if (barrasMostrar === 0){
         barrasMostrar=1;
-        console.log("Si mostrar");
     }
     else {
         barrasMostrar=0;
-        console.log("No mostrar");
     }
+    //MUESTRA U OCULTA LA COLA DE REPRODUCCIÓN
     if(barrasMostrar === 1){
         colaReproduccion.style.display = "flex";
     }
