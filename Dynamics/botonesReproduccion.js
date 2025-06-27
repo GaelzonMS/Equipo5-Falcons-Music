fb5fc70b864dc843a41a8f97fe0cc6734ff099e7
reginar-gh <324301890@alumno.enp.unam.mx>
2025-06-26 23:57:22 -0600

    funciona el buscador con reproductor de canciones

--- Dynamics/botonesReproduccion.js
+++ Dynamics/botonesReproduccion.js
@@ -1,12 +1,19 @@
 let player;
 let duracion = 0;
 let updateInterval;
-let videoId= "cWppAbqm9I8";
+//let videoId= "cWppAbqm9I8";
 
+
+if (!window.baseDatosJSON) {
+    console.error('baseDatosJSON no está definido');
+  }
+
+//const botonRepro = document.getElementById("reproducir-btn");
 const inputDuracion = document.getElementById("duracion");
 const playPauseBtn = document.getElementById("playPausa");
 let playPauseImg= document.getElementById("playImg");
 //const duracion = document.getElementById("duracion");
+
 
 function onPlayerReady(event) {
     duracion = player.getDuration();
@@ -32,22 +39,20 @@ function onPlayerStateChange(event){
     }
 }
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player("player", {
-        videoId: "cWppAbqm9I8",
-        playerVars: {
-            controls: 0,
-            modestbranding: 1,
-            rel: 0,
-            showinfo: 0,
-            fs: 0,
-        },
-        events: {
-            onReady: onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        },
-    });
-}
+//function onYouTubeIframeAPIReady() {
+//        videoId: "cWppAbqm9I8",
+//            controls: 0,
+//            modestbranding: 1,
+//            rel: 0,
+//            showinfo: 0,
+//            fs: 0,
+//        },
+//        events: {
+//            onReady: onPlayerReady,
+//            'onStateChange': onPlayerStateChange
+//        },
+//    });
+//}
 
 playPauseBtn.addEventListener("click", () => {
     let state = player.getPlayerState();
@@ -60,4 +65,28 @@ playPauseBtn.addEventListener("click", () => {
 inputDuracion.addEventListener("input", () => {
     let seekTo = inputDuracion.value;
     player.seekTo(seekTo, true);
+});
+
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('reproducir-btn')) {
+        const videoId = e.target.getAttribute('data-link');
+        if (player) {
+            player.loadVideoById(videoId);
+        } else {
+            player = new YT.Player("player", {
+                videoId: videoId,
+                playerVars: {
+                    controls: 0,
+                    modestbranding: 1,
+                    rel: 0,
+                    showinfo: 0,
+                    fs: 0,
+                },
+                events: {
+                    onReady: onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                },
+            });
+        }
+    }
 });
